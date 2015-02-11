643c5745c013cb68522b761993218811a69fb0c1
Will Tachau <wtachau@gmail.com>
2015-02-10 20:16:54 -0800

    fixed bug where logged out users crash

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,9 +3,11 @@ class SpacesController < ApplicationController
   def home
   	if !logged_in?
   		redirect_to '/login'
+  	else 
+  		@posts = Post.order('updated_at DESC').all
+	  	@relevant_posts = get_relevant_posts
   	end
-  	@posts = Post.order('updated_at DESC').all
-  	@relevant_posts = get_relevant_posts
+  	
   end
 
 end
