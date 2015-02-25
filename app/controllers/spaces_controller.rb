2afc83cfa9b7608ccfffba62695538e670998e13
Will Tachau <wtachau@gmail.com>
2015-02-24 16:40:02 -0800

    Decorator for user

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -5,7 +5,7 @@ class SpacesController < ApplicationController
   		redirect_to '/login'
   	else 
   		@posts = Post.order('updated_at DESC').all.decorate
-  		@current_user = current_user
+  		@current_user = current_user.decorate
 	  	@relevant_posts = ::PostsService.new(current_user).get_relevant_posts.decorate
   	end
   	
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_many :follows, dependent: :destroy
 	acts_as_taggable
-
-	def to_s
-		first_name + " " + last_name
-	end
 end
