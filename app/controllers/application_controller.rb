73b7b23426376fa573a390f451cc0ecc8aa6c201
Will Tachau <wtachau@gmail.com>
2015-04-10 17:18:38 -0700

    fixing current_user bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,25 @@ class ApplicationController < ActionController::Base
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
-	
-	before_action :current_user
-	skip_before_action :current_user, only: [:verify_logged_in, :login, :logged_in]
 
-	before_filter :verify_logged_in
+	before_action :verify_logged_in
 
 	def verify_logged_in
 		if !logged_in?
 			redirect_to '/login'
+		else
+			@current_user = current_user.decorate
 		end
-	end
-
-	# Logs in user
-	def log_in(user)
-		session[:user_id] = user.id
 	end
 
 	# Returns true if the user is logged in, false otherwise.
 	def logged_in?
 		!current_user.nil?
+	end
+
+	# Logs in user
+	def log_in(user)
+		session[:user_id] = user.id
 	end
 
 	# Logs out the current user.
@@ -31,8 +30,8 @@ class ApplicationController < ActionController::Base
 	end
 
 	protected
-		# Sets current user by lookup of session cookie
+		# Lookup current user by session cookie
 		def current_user
-			@current_user = User.find_by(id: session[:user_id]).decorate
+			User.find_by(id: session[:user_id])
 		end
 end
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,7 +2,6 @@ class SpacesController < ApplicationController
 	
 	def home
 		@posts = Post.order('updated_at DESC').all.decorate
-		@current_user = current_user.decorate
 		@relevant_posts = PostsService.new(current_user).relevant_posts_decorated
 	end
 
